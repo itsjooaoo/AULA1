63133de1c69d5e28d2aa54ee92d8a13f2a7a4cf9
labsfiap <labsfiap@PA205MICRO16.fiap.com.br>
2024-08-08 20:02:52 -0300

    My first commit in JBDC

---
+++ fiap.sql
@@ -0,0 +1,13 @@
+BEGIN
+ 
+    FOR X IN (
+        SELECT
+            table_name
+        FROM
+            user_tables
+    ) LOOP
+        EXECUTE IMMEDIATE 'DROP TABLE '
+                    || x.table_name
+                    || 'CASCADE CONSTRAINTS';
+        END LOOP;
+END;
